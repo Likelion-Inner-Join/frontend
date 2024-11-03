18819dd36049e71b8db3952fa683d6302ac9bfba
Saereem Lee <reems0815@gmail.com>
2024-11-03 20:59:53 +0900

    add post-write & post-manage pages

--- src/common/router/router.tsx
+++ src/common/router/router.tsx
@@ -1,8 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
-
+import PostManage from "../../owner/post_manage/post_manage";
+import PostWrite from "../../owner/post_write/post_write";
+import { PostProvider } from "../../owner/post_context/post_context";
+import FormBuilder from "../../owner/apply_form/form_builder";
 export const ROUTES = {
   SIGNUP: "/signup",
   LOGIN: "/login",
+  POST_MANAGE: "/post-manage",
+  POST_WRITE: "/post-write",
+  FORM_BUILDER: "/apply-form",
 };
 
 export const router = createBrowserRouter([
@@ -13,5 +19,25 @@ export const router = createBrowserRouter([
   {
     path: ROUTES.LOGIN,
     element: <></>,
+  },
+  {
+    path: ROUTES.POST_MANAGE, // PostManage 페이지 경로 추가
+    element: (
+      <PostProvider>
+        <PostManage />
+      </PostProvider>
+    ), // PostManage 컴포넌트를 해당 경로에 연결
+  },
+  {
+    path: ROUTES.POST_WRITE, // PostManage 페이지 경로 추가
+    element: (
+      <PostProvider>
+        <PostWrite />
+      </PostProvider>
+    ), // PostManage 컴포넌트를 해당 경로에 연결
+  },
+  {
+    path: ROUTES.FORM_BUILDER,
+    element: <FormBuilder />,
   },
 ]);
