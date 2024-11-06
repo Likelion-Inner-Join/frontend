fda97dca752b457a3522c8c667f08ddbea9f34bb
SEOA <tjdk2002@gmail.com>
2024-11-07 03:19:26 +0900

    Feat: Make email write page. References #3

---
+++ src/manager/mock/positionData.tsx
@@ -0,0 +1,14 @@
+export const positionData = [
+  {
+    id: "1",
+    name: "전체",
+  },
+  {
+    id: "2",
+    name: "단장단",
+  },
+  {
+    id: "3",
+    name: "기획단",
+  },
+];
--- src/manager/pages/DocEvaluate.tsx
+++ src/manager/pages/DocEvaluate.tsx
@@ -3,6 +3,8 @@ import styled from "styled-components";
 import ApplicantList from "../components/ApplicantList";
 import InformationBox from "../components/InformationBox";
 import ResultTable from "../components/ResultTable";
+import { applicantData } from "../mock/applicantData";
+import { positionData } from "../mock/positionData";
 
 interface Applicant {
   id: string;
@@ -11,136 +13,6 @@ interface Applicant {
   secondState: string;
   position: string;
 }
-
-const applicantData = [
-  {
-    id: "20211511",
-    name: "김서아",
-    firstState: "pass",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211512",
-    name: "나서아",
-    firstState: "pass",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211513",
-    name: "다서아",
-    firstState: "fail",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211514",
-    name: "라서아",
-    firstState: "null",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211515",
-    name: "마서아",
-    firstState: "fail",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211516",
-    name: "바서아",
-    firstState: "pass",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211517",
-    name: "사서아",
-    firstState: "null",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211518",
-    name: "아서아",
-    firstState: "null",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211519",
-    name: "자서아",
-    firstState: "pass",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211520",
-    name: "차서아",
-    firstState: "pass",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211521",
-    name: "카서아",
-    firstState: "pass",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211522",
-    name: "타서아",
-    firstState: "pass",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211523",
-    name: "파서아",
-    firstState: "pass",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211524",
-    name: "하서아",
-    firstState: "pass",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211525",
-    name: "김어아",
-    firstState: "pass",
-    secondState: "null",
-    position: "단장단",
-  },
-  {
-    id: "20211526",
-    name: "나어아",
-    firstState: "pass",
-    secondState: "null",
-    position: "단장단",
-  },
-];
-
-const positionData = [
-  {
-    id: "1",
-    name: "전체",
-  },
-  {
-    id: "2",
-    name: "단장단",
-  },
-  {
-    id: "3",
-    name: "기획단",
-  },
-];
 
 const DocEvaluate = () => {
   const [restList, setRestList] = useState<Applicant[]>([]);
