5f489ea3da30a9cb60bcdd843e2b76b2b288fd66
Saereem Lee <reems0815@gmail.com>
2024-11-06 20:16:18 +0900

    add apply-form & apply-manage pages

--- src/common/router/router.tsx
+++ src/common/router/router.tsx
@@ -2,13 +2,16 @@ import { createBrowserRouter } from "react-router-dom";
 import PostManage from "../../owner/post_manage/post_manage";
 import PostWrite from "../../owner/post_write/post_write";
 import { PostProvider } from "../../owner/post_context/post_context";
-import FormBuilder from "../../owner/apply_form/form_builder";
+import ApplyForm from "../../owner/apply_form/apply_form";
+import ApplyManage from "../../owner/apply_manage/apply_manage";
 export const ROUTES = {
   SIGNUP: "/signup",
   LOGIN: "/login",
   POST_MANAGE: "/post-manage",
   POST_WRITE: "/post-write",
   FORM_BUILDER: "/apply-form",
+  APPLY_FORM: "/apply-form",
+  APPLY_MANAGE: "/apply-manage",
 };
 
 export const router = createBrowserRouter([
@@ -21,23 +24,27 @@ export const router = createBrowserRouter([
     element: <></>,
   },
   {
-    path: ROUTES.POST_MANAGE, // PostManage 페이지 경로 추가
+    path: ROUTES.POST_MANAGE,
     element: (
       <PostProvider>
         <PostManage />
       </PostProvider>
-    ), // PostManage 컴포넌트를 해당 경로에 연결
+    ),
   },
   {
-    path: ROUTES.POST_WRITE, // PostManage 페이지 경로 추가
+    path: ROUTES.POST_WRITE,
     element: (
       <PostProvider>
         <PostWrite />
       </PostProvider>
-    ), // PostManage 컴포넌트를 해당 경로에 연결
+    ),
   },
   {
-    path: ROUTES.FORM_BUILDER,
-    element: <FormBuilder />,
+    path: ROUTES.APPLY_FORM,
+    element: <ApplyForm />,
+  },
+  {
+    path: ROUTES.APPLY_MANAGE,
+    element: <ApplyManage />,
   },
 ]);
