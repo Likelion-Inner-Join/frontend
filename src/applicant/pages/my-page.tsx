cb62bbae3333319259a914e8e15e4a887550fb29
Seoyoung Jung <wjdtjdud530@naver.com>
2024-12-29 02:21:05 +0900

    지원자 정보 조회

--- src/applicant/pages/my-page.tsx
+++ src/applicant/pages/my-page.tsx
@@ -1,10 +1,48 @@
 import { ApplicantPage } from "../page";
 import { BasicInfoSection, SchoolInfoSection } from "../components";
+import { useEffect, useState } from "react";
+import { GET } from "../../common/api/axios";
+
+export type TApplicationInfoData = {
+  name: string;
+  email: string;
+  school: string;
+  major: string;
+  studentNumber: string;
+};
 
 export const MyPage = () => {
+  const [data, setData] = useState<TApplicationInfoData>();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetch = async () => {
+      try {
+        setLoading(true);
+        const response = await GET("user");
+        if (response.isSuccess) {
+          setData(response.result);
+        } else {
+          throw new Error(response.message || "Failed to fetch posts");
+        }
+      } catch (err) {
+        console.log(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetch();
+  }, []);
+
+  if (!data) {
+    return <div>로딩 중...</div>;
+  }
+
   return (
     <ApplicantPage>
-      <BasicInfoSection />
+      {loading && <div>로딩 중...</div>}
+      <BasicInfoSection name={data.name} email={data.email} />
       <div
         style={{
           height: "1px",
@@ -13,7 +51,11 @@ export const MyPage = () => {
           width: "912px",
         }}
       />
-      <SchoolInfoSection />
+      <SchoolInfoSection
+        school={data.school}
+        major={data.major}
+        studentNumber={data.studentNumber}
+      />
     </ApplicantPage>
   );
 };
